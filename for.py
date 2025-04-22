0972646e9ff68321089acb6df63cdea5a380892b
NETTAX-NT <nataf2057@gmail.com>
2025-04-22 16:24:38 -0400

    ejercicios con for

---
+++ for.py
@@ -0,0 +1,55 @@
+#  explicasion de uso de for
+
+# for i in range(3):
+#     print("hola")
+# num=int(input("ingrese un numero "))
+
+# for i in range(num):
+#     print("repeticione", i+1)
+
+# nombre=input("ingrese su nombre ")
+# edad=input("ingrese su edad ")
+# print(f"su nombre es {nombre} \nsu edad {edad}")
+
+#  escribit =print
+# leer = input
+# si,=if
+# para = for  
+# tabla de multplicar
+# num=in(input("ingrese un numero "))
+# for i in range(1.11):
+#     print()
+
+# for i in range(1,11):
+#     print("tabla del", i)
+#     for j in range(1.11):
+#         print(f"{i} x {j} = {i*j}")
+
+# pedir al usuario la cantidad de notas y promediarla
+# can=int(input("ingrese la cantidad de notas "))
+# total=0
+# notasazul=0
+# for i in range(can):
+#     print("ingrese la nota", i+1)
+#     nota=float(input())
+#     total=total+nota
+#     prom=total/can
+#     if nota>=4:
+#         notasazul+=1
+# print(f"su promedio es {round(prom,1)}")
+# print(f"la cantidad de notas sobre 4 fue {notasazul}")
+
+# if prom>=4:
+#     print("aprobo")
+# else:
+#     print("reprobo")
+
+# for i in range(3):
+#     clave=2123
+#     pas=int(input("ingrese la clave"))
+#     if pas==clave:
+#         print("vien venido")
+#         break
+#     else:
+#         print("erro de clave")
+
