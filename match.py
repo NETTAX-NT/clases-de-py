0ad721860861053f654140579a9fc9c38b6d448c
NETTAX-NT <nataf2057@gmail.com>
2025-05-16 18:46:46 -0400

    match eje

--- match.py
+++ match.py
@@ -68,10 +68,80 @@
 # una excepción o no
 # print("Este bloque se ejecuta siempre")
 
-def prom():
-    n1=int(input("ingrese su nota "))
-    n2=int(input("ingrese su nota "))
-    n3=int(input("ingrese su nota "))
-    suma=(n1+n2+n3)/3
-    print(f"su promedio es {suma}")
-def calculadora
+# def prom():
+#     n1=int(input("ingrese su nota "))
+#     n2=int(input("ingrese su nota "))
+#     n3=int(input("ingrese su nota "))
+#     suma=(n1+n2+n3)/3
+#     print(f"su promedio es {suma}")
+# def calculadora
+
+# total=0
+# to=0
+# cantidad=0
+
+# def user():
+#     global nom
+#     nom=input("ingrese su nombre: ")
+# def pro():
+#     global cantidad,total,to
+
+    
+#     produc=int(input('''selecsione un producto
+#                      1.-confor 1500
+#                      2.-sepillo de dientes 1000
+#                      3.-pasta de diente 2000
+#                      '''))
+#     match produc:
+#         case 1:
+#             total+=1500
+#             to+=(1500*1.19)
+#             cantidad+=1     
+#         case 2:
+#             total+=1000
+#             to+=(1500*1.19)
+#             cantidad+=1  
+#         case 3:
+#             total+=2000
+#             to+=(1500*1.19)
+#             cantidad+=1  
+# def bol():
+#     print(f"usuario {nom}")
+#     print("productos totales", cantidad)
+#     print(f"presio sin iva {total} con iva {round(to)}")
+# def cat()        
+#     while True:
+#         op=int(input('''escoja una opcion
+#                     1.-ingrese su nombre
+#                     2.-selecsionar productos
+#                     3.-imprimir boletas
+#                     4.-salir
+#                     '''))
+#         match op:
+#             case 1:
+#                 user()
+#             case 2:
+#                 pro()
+#             case 3:
+#                 bol()
+#             case 4:
+#                 print("saliendo")
+#                 break
+# # cat()
+
+alumnos=int(input("ingrese la cantidad de alumnos: "))
+mientras=0
+for i in range(alumnos):
+    i+=1
+    mientras=0
+    nota=int(input("ingrese la cantidad de notas: ")) 
+    for e in range(nota):
+        e+=1
+        notas=int(input(f"ingrese la nota {e} alumno {i}: "))
+        mientras+=notas
+        promedio=mientras/nota
+    if promedio>=4.0:
+        print(f"aprobaste tu nota es {promedio}")
+    else:
+        print(f"reprobaste {promedio}")
+
